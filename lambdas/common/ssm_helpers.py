e7eb4dd8b2898ad5e02a326a636cb3a2ae74b68f
JarvisXomware <jarvis@xomware.com>
2026-03-01 12:03:35 -0500

    fix: remove unused AWS SSM params from ssm_helpers causing ParameterNotFound crash
    The /xomify/aws/ACCESS_KEY and /xomify/aws/SECRET_KEY SSM parameters
    don't exist and were never actually imported/used by any Lambda function.
    They were dead code that caused a fatal crash at module import time,
    breaking all Lambdas that import from the shared layer (including
    xomify-cron-wrapped).

--- lambdas/common/ssm_helpers.py
+++ lambdas/common/ssm_helpers.py
@@ -3,13 +3,8 @@ from lambdas.common.constants import PRODUCT
 
 ssm = boto3.client("ssm", verify=False)
 
-__AWS_ROOT = f'/{PRODUCT}/aws/'
 __SPOTIFY_ROOT = f'/{PRODUCT}/spotify/'
 __API_ROOT = f'/{PRODUCT}/api/'
-
-# AWS
-AWS_ACCESS_KEY = ssm.get_parameter(Name=f'{__AWS_ROOT}ACCESS_KEY', WithDecryption=True)['Parameter']['Value']
-AWS_SECRET_KEY = ssm.get_parameter(Name=f'{__AWS_ROOT}SECRET_KEY', WithDecryption=True)['Parameter']['Value']
 
 # SPOTIFY
 SPOTIFY_CLIENT_ID = ssm.get_parameter(Name=f'{__SPOTIFY_ROOT}CLIENT_ID', WithDecryption=True)['Parameter']['Value']
